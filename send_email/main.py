67a79b4fbc10d9c076e627d3afa0edf150a3c6a1
Linus Frost <86301455+Arecius88@users.noreply.github.com>
2025-02-18 21:15:19 +0100

    send email klar.

--- send_email/main.py
+++ send_email/main.py
@@ -1,16 +1,42 @@
 import smtplib
-from email.message import EmailMessage
+import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
 
-textfile = "send_email/message.txt"
+# Load environment variables
+load_dotenv()
 
-with open(textfile) as fp:
-    msg = EmailMessage()
-    msg.set_content(fp.read())
+# Email Config
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+EMAIL_ADRESS= os.getenv("EMAIL_ADRESS")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
-msg["Subject"] = f"The contents of {textfile}"
-msg["From"] = "arecius88.mail@gmail.com"
-msg["To"] = "arecius88.mail+temp@gmail.com"
-
-s = smtplib.SMTP("localhost")
-s.send_message(msg)
-s.quit()
+def send_email(recipient_email:str, title:str, body:str)->None:
+    
+    try:
+        message = MIMEMultipart()
+        message["From"] = EMAIL_ADRESS
+        message["To"] = recipient_email
+        message["Subject"] = title
+        
+        message.attach(MIMEText(body, "plain"))
+        
+        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+            server.starttls()
+            server.login(EMAIL_ADRESS, EMAIL_PASSWORD)
+            server.sendmail(EMAIL_ADRESS, recipient_email, message.as_string())
+            print("Email sent successfully")
+    except Exception as e:
+        print(f"Error: {e}")
+        
+        
+if __name__ == "__main__":
+    recipient_email = "arecius88.mail@gmail.com"
+    title = "Test Email"
+    body = "This is a test email"
+    send_email(recipient_email, title, body)
+    
+    #print(EMAIL_ADRESS)
+    #print(EMAIL_PASSWORD)
